731a5e7e4068507b8487f80da4f629cca473fc8f
Michael Mano <michael.mano26@gmail.com>
2017-12-21 16:48:53 +1000

    Removes secure config keys out of the prod.exs and into a secret.

--- config/config.exs
+++ config/config.exs
@@ -9,19 +9,6 @@ use Mix.Config
 config :interface,
   ecto_repos: [Interface.Repo]
 
-# Configures the endpoint
-config :interface, InterfaceWeb.Endpoint,
-  url: [host: "localhost"],
-  secret_key_base: "+oxVNYzCtJy5QYMS+xkl+eirNN0RnPiGnVJR9e2kmzxgD/LceGnRyOuzrVDz9AX5",
-  render_errors: [view: InterfaceWeb.ErrorView, accepts: ~w(json)],
-  pubsub: [name: Interface.PubSub,
-           adapter: Phoenix.PubSub.PG2]
-
-# Configures Elixir's Logger
-config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
-
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
