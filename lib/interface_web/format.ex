ba4379b32bf96bf3093dfff7734ebe245625b826
Michael Mano <michael.mano26@gmail.com>
2018-01-22 11:26:22 +1000

    Refactors Helpers to Format

--- lib/interface_web/format.ex
+++ lib/interface_web/format.ex
@@ -4,7 +4,6 @@ defmodule InterfaceWeb.Format do
   Simple helper functions that do not require their own module.
   """
 
-  @doc false
   def init(opts), do: opts
   
   def mapify({k,v}, m), do: Map.put(m, k, v)
