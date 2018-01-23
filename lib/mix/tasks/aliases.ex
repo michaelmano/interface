a987bfa7d024d3f3ec8bee8c11741de9e9787cd5
Michael Mano <michael.mano26@gmail.com>
2018-01-24 09:12:56 +1000

    Removes run.sh as no longer required and adds ecto.reset and ecto.setup to the aliases.ex file

--- lib/mix/tasks/aliases.ex
+++ lib/mix/tasks/aliases.ex
@@ -1,7 +1,21 @@
 # This file is for adding short docs for mix aliases so they show up in the mix help command output.
 defmodule Mix.Tasks.Watch do
   use Mix.Task
-  @shortdoc "Alias for import.env and phx.server"
+  @shortdoc "Alias: ['import.env', 'phx.server']"
+  def run(_) do
+  end
+end
+
+defmodule Mix.Tasks.Ecto.Setup do
+  use Mix.Task
+  @shortdoc "Alias: ['ecto.create', 'ecto.migrate', 'run priv/repo/seeds.exs']"
+  def run(_) do
+  end
+end
+
+defmodule Mix.Tasks.Ecto.Reset do
+  use Mix.Task
+  @shortdoc "Alias: ['ecto.drop', 'ecto.setup']"
   def run(_) do
   end
 end
---
+++ lib/mix/tasks/check_configs.ex
@@ -0,0 +1,6 @@
+defmodule Mix.Tasks.Import.Configs do
+  use Mix.Task
+  def run(_) do
+    IO.puts("TODO: Check if /config/*.secret.exs files exist and if not copy over the examples.")
+  end
+end
